94d1d8f60dbf5a4e6d651ada64e0ee9d85836fab
Stefan Jaroni <sjaroni@outlook.de>
2025-03-08 11:51:56 +0100

    add optional query-parameter

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,21 +1,51 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { EventsService } from './events.service';
 
 @Controller('events')
 export class EventsController {
-
-  constructor(private eventService: EventsService){}
+  constructor(private eventService: EventsService) {}
 
   @Get()
-  findAll(): any[]{
+  findAll(): any[] {
     return this.eventService.findAll();
   }
   // http://localhost:3000/events
 
-  @Get(':id')
-findOne(@Param('id') id: string): any {
-  return this.eventService.findAll().find(event => event.id === parseInt(id));
-}
-  // http://localhost:3000/events/2
-}
 
+  // Path
+
+  @Get(':id')
+  findOne(@Param('id') id: string): any {
+    return this.eventService
+      .findAll()
+      .find((event) => event.id === parseInt(id));
+  }
+  // http://localhost:3000/events/2
+
+  // @Get(':name')
+  // findOne(@Param('name') name: string): any {
+  //   return this.eventService
+  //     .findAll()
+  //     .find((event) => event.name === decodeURIComponent(name));
+  // }
+
+  //http://localhost:3000/events/Angular%20Meetup%20Hamburg
+
+  // Query-Parameter
+  // @Get()
+  // findAll(@Query('name') name?: string): any[] | any {
+  //   const events = this.eventService.findAll();
+
+  //   if (name) {
+  //     return events.find((event) => event.name === name);
+  //   }
+
+  //   return events; // Falls kein Name angegeben wird, alle Events zurückgeben
+  // }
+
+  // http://localhost:3000/events?name=Angular Meetup Hamburg
+  //   Vorteile der Query-Parameter-Methode:
+  // ✔ Bessere Lesbarkeit
+  // ✔ Kein Encoding/Decoding nötig
+  // ✔ Saubere URLs
+}
