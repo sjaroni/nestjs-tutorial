554cfb378d37b9e0bbe40873e6905aa500f6dc02
Stefan Jaroni <sjaroni@outlook.de>
2025-03-08 11:37:09 +0100

    add @GET-Decorator to return values from static array

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { EventsService } from './events.service';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+
+  constructor(private eventService: EventsService){}
+
+  @Get()
+  findAll(): any[]{
+    return this.eventService.findAll();
+  }
+  // http://localhost:3000/events
+
+  @Get(':id')
+findOne(@Param('id') id: string): any {
+  return this.eventService.findAll().find(event => event.id === parseInt(id));
+}
+  // http://localhost:3000/events/2
+}
+
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+  private events = [
+    { id: 1, name: 'Tech Conference 2025' },
+    { id: 2, name: 'Angular Meetup Hamburg' },
+    { id: 3, name: 'NestJS Workshop Berlin' }
+  ];
+
+  findAll() {
+    return this.events;
+  }
+}
