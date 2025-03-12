52e058db9f44002073272d8aec365cc058dc3f9b
Stefan Jaroni <sjaroni@outlook.de>
2025-03-12 22:59:56 +0100

    feat: connect to postgresql-database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EventsModule],
+  imports: [EventsModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'tutorial',
+      database: 'tutorial',
+      entities: ["src/**/*.entity{.ts,.js}"],
+      // entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
